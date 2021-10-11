7fa9a35ef2791539938789294b2857fc3a900f5f
clau-ale <claudiom.ale22@gmail.com>
2021-10-11 17:55:52 -0300

    primer commit

---
+++ Tarea-Seis/tareaPd.py
@@ -0,0 +1,11 @@
+#Parte 2
+texto = input("Ingrese un texto...")
+letras = input("Ingrese CINCO LETRAS...")
+letras = list(letras)
+texto = texto.replace("a",letras[0])
+texto = texto.replace("e",letras[1])
+texto = texto.replace("i",letras[2])
+texto = texto.replace("o",letras[3])
+texto = texto.replace("u",letras[4])
+texto = texto.split(" ")
+print(texto)
---
+++ Tarea-Seis/tareaPu.py
@@ -0,0 +1,8 @@
+#Parte 1
+texto = input("Ingrese un texto...")
+texto = texto.replace("a"," ")
+texto = texto.replace("e"," ")
+texto = texto.replace("i"," ")
+texto = texto.replace("o"," ")
+texto = texto.replace("u"," ")
+print(texto)
